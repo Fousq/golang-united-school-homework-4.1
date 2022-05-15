1ba623fc4710cc26757b21063f5dab91525182e5
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-15 18:15:00 +0600

    Add implementation

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	outputByte := make([]byte, 0, len(input))
+	start := len(input) - 1
+	for i := start; i >= 0; i-- {
+		outputByte[start-i] = input[i]
+	}
+	output = string(outputByte)
 	return output
 }
