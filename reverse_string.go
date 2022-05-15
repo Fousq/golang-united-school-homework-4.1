47bd66ad799282a5ebe6d5e27359af4f99ffa8e8
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-15 18:28:15 +0600

    Fix an issue

--- reverse_string.go
+++ reverse_string.go
@@ -7,8 +7,7 @@ func ReverseString(input string) (output string) {
 	outputByte := make([]byte, 0, len(input))
 	start := len(input) - 1
 	for i := start; i >= 0; i-- {
-		outputByte[start-i] = input[i]
+		outputByte = append(outputByte, input[i])
 	}
-	output = string(outputByte)
-	return output
+	return string(outputByte)
 }
