b78ea20e548f650b65ba55386727670455c0eeab
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-15 18:18:08 +0600

    Add length checking

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
+	if len(input) < 2 {
+		return input
+	}
 	outputByte := make([]byte, 0, len(input))
 	start := len(input) - 1
 	for i := start; i >= 0; i-- {
