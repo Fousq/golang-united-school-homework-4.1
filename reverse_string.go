8f2a3cd12f8cee81ffe2b6abd47790ac2ec0bf36
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-15 18:43:06 +0600

    Change the order

--- reverse_string.go
+++ reverse_string.go
@@ -4,9 +4,9 @@ func ReverseString(input string) (output string) {
 	if len(input) < 2 {
 		return input
 	}
-	outputRune := make([]rune, 0, len(input))
 	inputRune := []rune(input)
-	start := len(input) - 1
+	outputRune := make([]rune, 0, len(inputRune))
+	start := len(inputRune) - 1
 	for i := start; i >= 0; i-- {
 		outputRune = append(outputRune, inputRune[i])
 	}
