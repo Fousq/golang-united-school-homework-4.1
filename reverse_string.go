0dbaad0d687dfdab49d52ec8038d1bbc441a0d0e
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-15 18:33:28 +0600

    Use rune insted of byte

--- reverse_string.go
+++ reverse_string.go
@@ -4,10 +4,11 @@ func ReverseString(input string) (output string) {
 	if len(input) < 2 {
 		return input
 	}
-	outputByte := make([]byte, 0, len(input))
+	outputRune := make([]rune, 0, len(input))
+	inputRune := []rune(input)
 	start := len(input) - 1
 	for i := start; i >= 0; i-- {
-		outputByte = append(outputByte, input[i])
+		outputRune = append(outputRune, inputRune[i])
 	}
-	return string(outputByte)
+	return string(outputRune)
 }
